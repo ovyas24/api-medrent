05bcb3f47c480c2c2cea44dea58734a08bafd654
ovyas24 <ovyas24@gmail.com>
2021-04-16 17:45:28 +0530

    Added front fucntionality

---
+++ controllers/frontController.js
@@ -0,0 +1,31 @@
+const Category = require("../model/category");
+const Product = require("../model/product");
+
+class FrontRepo {
+    constructor() { }
+    async SingleProduct(id) {
+        try {
+            const pro = await Product.findOne({ _id: id })
+            const cat = await Category.findOne({ products: { $in: [id] } })
+            let { name, price, description, image, date } = pro
+            let catname = cat.name, catdesc = cat.description
+            const product = {
+                name, price, description, image, date,
+                catname,
+                catdesc
+            }
+            return product
+
+        } catch (error) {
+            console.log(error);
+            return error
+        }
+    }
+
+    async NewRelaeses() {
+        const products = Product.find().limit(4).sort({ $natural: -1 })
+        return products
+    }
+}
+
+module.exports = new FrontRepo()
--- routes/user/index.js
+++ routes/user/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Repo = require('../../controllers/dashboard')
+const FrontRepo = require('../../controllers/frontController')
 const router = express.Router()
 
 
@@ -8,7 +9,22 @@ router.get("/shop", async (req,res) => {
     const products = await Repo.Shop(req.query.search)
     res.json(products)
 })
-router.get("/product/:id")
-router.get("/newRelease") 
+router.get("/product/:id", async (req,res)=>{
+    try {
+        console.log(req.params.id);
+        const product = await FrontRepo.SingleProduct(req.params.id)
+        res.json(product)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+router.get("/newRelease", async (req,res)=>{
+    try {
+        const products = await FrontRepo.NewRelaeses()
+        res.json(products)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}) 
 
 module.exports = router
